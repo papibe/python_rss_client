e7fb3c1e0d9ad3f21efd164f9a8755ad3701bb3e
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2015-06-04 21:40:33 -0500

    Change Config as module
    rss.py was moved to rrslibrary and a new main.py is the main calling
    program now.

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+# Copyright (C) 2015 Pablo Piaggio.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import curses
+import feedparser
+import os
+import json
+import sys
+
+#import rsslibrary.config
+from rsslibrary import config
+
+def main( argv ):
+    # Create config object and load the file.
+    try:
+        feed_config = config.Config( config.CONFIG_FILE )
+    except Exception as e:
+        print( e, file=sys.stderr)
+        return
+    else:
+        if hasattr( feed_config, "warnings"):
+            print( feed_config.warnings, file=sys.stderr)
+
+    print( feed_config, end="" )
+    return
+
+if __name__ == "__main__":
+    main( sys.argv )
